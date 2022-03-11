8d0fee43ae1009076288f00581c2079f664a7c49
oheneadj <oheneadjei.dev@gmail.com>
2022-03-11 15:04:41 +0000

    my first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// This is a Javascript file
+let firstName = "Ohene Adjei";
+let lastName = "Effah";
+let age = 27;
+
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old`);
+
+let fruits = ["Mango", "Orange", "Banana"];
+
+console.log(fruits[0]);
+console.log(fruits.length);
+
+let person = {
+    firstName:"Ohene",
+    lastName:"Effah",
+    age:20
+    
+}
+
+console.log(`My name is ${person.firstName} and ${person.lastName} is my surname.`);
+
+// // console.table(person);
+// // console.table(fruits);
+// console.log(fruits.shift("Apple"));
+// console.table(fruits.unshift("Apple"));
+
+
+
+
